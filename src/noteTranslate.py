cd42676e2f3f33210736c8da8b4841ef3e8633ee
bope0915 <bogdan.innova@gmail.com>
2016-04-14 12:41:31 +0300

    Simple working bot-translator

--- src/noteTranslate.py
+++ src/noteTranslate.py
@@ -3,31 +3,23 @@ import src.config
 import telebot
 import time
 import urllib.request
+from urllib.parse import quote
 import xml.etree.ElementTree as ET
-
-
 
 def listener(messages):
     for m in messages:
         if m.content_type == 'text':
-            # url = ('https://translate.yandex.net/api/v1.5/tr/translate?key='+src.config.yandexToken+'&text='+m.text+'&lang=en').encode('utf-8')
-
-            userText = m.text.encode('utf-8')
-            userText = m.text.encode('ansii')
-            print(userText)
-            url = 'https://translate.yandex.net/api/v1.5/tr/translate?key=' + src.config.yandexToken + '&text='+userText+'&lang=en'
-
-            # url.encode('utf-8', 'ignore').decode('utf-8')
-            # url.encode('ascii', 'ignore')
-            # url.encode('ascii', 'replace')
-            # url.encode('ascii', 'xmlcharrefreplace')
-
-
-            print(url)
+            if m.text[0] == '/':
+                userText = m.text[1:]
+            else:
+                userText = m.text
+            url = 'https://translate.yandex.net/api/v1.5/tr/translate?key='\
+                  + src.config.yandexToken + '&text='+quote(userText)+'&lang=en'
             f = urllib.request.urlopen(url)
-            # root = ET.fromstring(f.read().decode('utf-8'))
-            # for translation in root.iter('Translation'):
-            #     bot.send_message(m.chat.id, translation.getText())
+            root = ET.fromstring(f.read().decode('utf-8'))
+            for tr in root.iter():
+                if tr.tag == 'text':
+                    bot.send_message(m.chat.id, tr.text)
 
 if __name__ == '__main__':
     bot = telebot.TeleBot(src.config.botToken)
